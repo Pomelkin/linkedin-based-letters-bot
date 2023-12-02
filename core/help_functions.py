fdc4184ed407452b55e843167ce01e170d7952b8
Egorov E.A <emelya.kara@yandex.ru>
2023-12-02 12:56:31 +0500

    Cleaning scripts

--- core/help_functions.py
+++ core/help_functions.py
@@ -1,13 +1,6 @@
 from contextlib import suppress
 from aiogram.exceptions import TelegramBadRequest
 from aiogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
-from moviepy.editor import *
-
-
-async def MP4ToMP3(mp4, mp3):
-    FILETOCONVERT = AudioFileClip(mp4)
-    FILETOCONVERT.write_audiofile(mp3)
-    FILETOCONVERT.close()
 
 
 def get_keyboard():
