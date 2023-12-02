6ea87422986d441034c59a843519b8de1a93643c
Egorov E.A <emelya.kara@yandex.ru>
2023-12-02 19:46:27 +0500

    Add little fix

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from aiogram import Bot, Dispatcher
-from core.config import TELEGRAM_TOKEN
+from core.config import TELEGRAM_TOKEN, MODE, WEBHOOK_URL, PORT
 from core.handlers.command_handlers import *
 from core.handlers.message_handlers import *
 from core.loader import dp, bot
@@ -9,11 +9,19 @@ import logging
 TELEGRAM_API_KEY = TELEGRAM_TOKEN
 
 
+async def on_startup(dp):
+    if MODE == "webhook":
+        await bot.set_webhook(WEBHOOK_URL + '/' + TELEGRAM_TOKEN)
+        logging.info(f"Start webhook mode on port {PORT}")
+    else:
+        logging.info(f"Start polling mode")
+
+
 async def start_bot():
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - [%(levelname)s] - %(name)s - %(message)s')
 
     try:
-        await dp.start_polling(bot)
+        await dp.start_polling(bot, on_startup=on_startup, skip_updates=True)
     finally:
         await bot.session.close()
 
